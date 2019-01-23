4e2ae712f29513e2637c8679069d444a3422becc
cooqi <407997152@qq.com>
2019-01-23 17:57:15 +0800

    调整

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -23,6 +23,31 @@
     margin: 0 auto;
     margin-right: 20px;
   }
+  .menu-item span{
+    display: inline-block;
+    overflow: hidden;
+    width: 69px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    vertical-align: bottom;
+    transition: width .2s ease .2s;
+  }
+  .menu-item i{
+    transform: translateX(0px);
+    transition: font-size .2s ease, transform .2s ease;
+    vertical-align: middle;
+    font-size: 16px;
+  }
+  .collapsed-menu span{
+    width: 0px;
+    transition: width .2s ease;
+  }
+  .collapsed-menu i{
+    transform: translateX(5px);
+    transition: font-size .2s ease .2s, transform .2s ease .2s;
+    vertical-align: middle;
+    font-size: 22px;
+  }
 </style>
 <template>
   <div class="layout">
@@ -52,32 +77,19 @@
       </Header>
       <Layout>
         <Sider hide-trigger :style="{background: '#fff'}">
-          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>
-                Item 1
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template slot="title">
-                <Icon type="ios-keypad"></Icon>
-                Item 2
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template slot="title">
-                <Icon type="ios-analytics"></Icon>
-                Item 3
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
+          <Menu active-name="1"  width="auto" :class="menuitemClasses">
+            <MenuItem name="1">
+              <Icon type="ios-navigate"></Icon>
+              <span>个人作品</span>
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="search"></Icon>
+              <span>个人信息</span>
+            </MenuItem>
+            <MenuItem name="3">
+              <Icon type="settings"></Icon>
+              <span>Tag管理</span>
+            </MenuItem>
           </Menu>
         </Sider>
         <Layout :style="{padding: '0 24px 24px'}">
@@ -96,6 +108,19 @@
 </template>
 <script>
     export default {
+        data () {
+            return {
+                isCollapsed: false
+            };
+        },
+        computed: {
+            menuitemClasses: function () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        }
 
     }
 </script>
